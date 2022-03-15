98fe8dc58a90c661a47e98fa26995743f3165276
Popescu Stefan <stefan.popescu@student.fontys.nl>
2022-03-15 12:52:03 +0200

    changes

---
+++ ZooBazaar/ZooBazaar/DataAccess.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace ZooBazaar
+{
+    public class DataAccess
+    {
+        protected MySqlConnection connection;
+        protected MySqlCommand command;
+        protected MySqlDataReader dataReader;
+        protected string query = "";
+
+        public DataAccess()
+        {
+
+
+            string database = "Server=studmysql01.fhict.local;Uid=dbi467287;Database=dbi467287;Pwd=prj; SSL Mode=None;";
+
+            this.connection = new MySqlConnection(database);
+        }
+
+
+        public void AddWithValue(string parameterName, object value)
+        {
+            this.command.Parameters.AddWithValue(parameterName, value);
+        }
+
+        public void NonQueryEx()
+        {
+            this.command.ExecuteNonQuery();
+        }
+
+        public void Close()
+        {
+            this.connection.Close();
+        }
+        public bool ConnOpen()
+        {
+            try
+            {
+                this.connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+
+                        MessageBox.Show("Can not connect (" + ex.Message + ")");
+
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Error in Database");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        public void SqlQuery(string queryText)
+        {
+            this.command = new MySqlCommand(queryText, this.connection);
+        }
+
+    }
+}
---
+++ ZooBazaar/ZooBazaar/Workshift.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooBazaar
+{
+    public class WorkShift
+    {
+        public int EmployeeId { get; private set; }
+        public string EmployeeName { get; private set; }
+        public string Date { get; private set; }
+        public string Type { get; private set; }
+        public decimal WageForShift { get; private set; }
+        public int HoursWorked { get; private set; }
+        public int ID { get; set; }
+
+        public bool Cancelled { get; private set; }
+
+        public WorkShift(int employeeId, string employee, string date, string type, decimal wage, int hours)
+        {
+            this.EmployeeId = employeeId;
+            this.EmployeeName = employee;
+            this.Date = date;
+            this.Type = type;
+            this.WageForShift = wage;
+            this.HoursWorked = hours;
+            this.Cancelled = false;
+        }
+
+        public WorkShift(int employeeId, string type, int hours)
+        {
+            this.EmployeeId = employeeId;
+            this.Type = type;
+            this.HoursWorked = hours;
+        }
+
+        public bool CancelShift()
+        {
+            if (!this.Cancelled)
+            {
+                this.Cancelled = true;
+                return true;
+            }
+            else { return false; }
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + this.ID + ", Employee ID:" + this.EmployeeId + " - " + this.EmployeeName;
+        }
+    }
+
+}
