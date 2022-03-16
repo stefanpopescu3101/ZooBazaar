5f34de75efb74f6f963d5e473c6c47cb7a03e9f9
Andrei Ţifrea <I455422@fhict.nl>
2022-03-16 12:02:42 +0100

    Employee management (with mediator)

---
+++ Class-Library/Managers/EmployeeManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Class_Library;
+using Class_Library.Data_Access;
+
+namespace Class_Library.Managers
+{
+    class EmployeeManager
+    {
+        private readonly EmployeeDb employeeDb;
+        private static int id = 0;
+        public List<Employee> employees { get; set; }
+        //EmployeesMediator employeesMediator;
+
+        public EmployeeManager()
+        {
+            employees = new List<Employee>();
+            employeeDb = new EmployeeDb();
+            Load();
+        }
+        public void GenerateUsernameAndPassword(Employee emp)
+        {
+            string userName = emp.FirstName.Substring(0, 1) + emp.LastName.Substring(0, 1) + emp.Id;
+            emp.Username = userName;
+            emp.Password = emp.Id + id++.ToString();
+            //employeesMediator.UpdateUsernameAndPassword(emp);
+
+        }
+        public Employee CheckCredentials(string username, string password)
+        {
+            foreach (Employee emp in employees)
+            {
+                if (emp.Username == username && emp.Password == password)
+                {
+                    return emp;
+                }
+            }
+            return null;
+        }
+        public int AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+            int newID = employeeDb.AddEmployee(employee);
+            return newID;
+        }
+
+        public void RemoveEmployee(Employee emp)
+        {
+            //employeesMediator.RemoveEmployee(emp);
+            employees.Remove(emp);
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            foreach (Employee e in GetEmployees())
+            {
+                if (e.Id == id)
+                {
+                    return e;
+
+                }
+            }
+            return null;
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+        public List<Employee> GetEmployees()
+        {
+            //employees = employeesMediator.GetEmployees();
+            return employees;
+        }
+        public void Load()
+        {
+            //employees = employeesMediator.GetEmployees();
+        }
+        /*/public List<Employee> GetEmployeeOfDepartment(Department depart)
+        {
+            return employeesMediator.GetEmployeesOfDepartment(depart);
+        }
+        
+        public void UpdateRoleAndDepartment(Employee emp, string department, string Role)
+        {
+            employeesMediator.UpdateRoleAndDepartment(emp, department, Role);
+        }
+        /*/
+        public void Update(Employee employee, string firstName, string lastName, string email,
+            string contractType, string address,
+            string department)
+        {
+            //employee.UpdateInfo(firstName, lastName, email, contractType, address, department);
+           // employeesMediator.Update(employee);
+        }
+
+        public void TerminateEmployeesContract(Employee employee, string endDate, string reason)
+        {
+            //employeesMediator.TerminateContract(employee, endDate, reason);
+            //employee.TerminateContract(reason, endDate);
+        }
+    }
+}
--- Class-Library/Object Classes/Employee.cs
+++ Class-Library/Object Classes/Employee.cs
@@ -1,12 +1,47 @@
-﻿/// <summary>
-///  temporary class, to be replaced
-/// </summary>
-namespace Class_Library.Data_Access
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Library
 {
     public class Employee
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string BSN { get; set; }
+        public string Email { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ContractType { get; set; }
+        public int HourlyWage { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+        public string DepartureReason { get; set; }
+        public int ShiftsPerWeek { get; set; }
+
+        public Employee(int id, string firstName, string lastName,string username, string password, string bsn, string email, DateTime startDate, DateTime endDate, DateTime birthdate, string contractType, int hourlyWage, string address, string role, string departureReason, int shiftsPerWeek)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Username = username;
+            this.Password = password;
+            this.BSN = bsn;
+            this.Email = email;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            this.Birthdate = birthdate;
+            this.ContractType = contractType;
+            this.HourlyWage = hourlyWage;
+            this.Address = address;
+            this.Role = role;
+            this.DepartureReason = departureReason;
+            this.ShiftsPerWeek = shiftsPerWeek;
+        }
+
     }
 }
