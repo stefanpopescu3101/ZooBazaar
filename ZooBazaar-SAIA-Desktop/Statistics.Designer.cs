a5dd024d9d0e5333a6210e9dfd1d935e72f92f03
Koornstra,Ard A.P.C <ard.koornstra@student.fontys.nl>
2022-06-01 03:04:31 +0200

    Statistics fully implemented for employees, animals, habitats

--- ZooBazaar-SAIA-Desktop/Statistics.Designer.cs
+++ ZooBazaar-SAIA-Desktop/Statistics.Designer.cs
@@ -27,11 +27,19 @@ namespace ZooBazaar_SAIA_Desktop {
             this.btnBack = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabEmployees = new System.Windows.Forms.TabPage();
-            this.pvwGender = new OxyPlot.WindowsForms.PlotView();
+            this.pvwContract = new OxyPlot.WindowsForms.PlotView();
+            this.pvwWages = new OxyPlot.WindowsForms.PlotView();
+            this.pvwEmployeeAge = new OxyPlot.WindowsForms.PlotView();
             this.tabAnimals = new System.Windows.Forms.TabPage();
+            this.pvwAge = new OxyPlot.WindowsForms.PlotView();
+            this.pvwPredator = new OxyPlot.WindowsForms.PlotView();
+            this.pvwHealth = new OxyPlot.WindowsForms.PlotView();
             this.tabHabitats = new System.Windows.Forms.TabPage();
+            this.pvwHabitats = new OxyPlot.WindowsForms.PlotView();
             this.tabControl1.SuspendLayout();
             this.tabEmployees.SuspendLayout();
+            this.tabAnimals.SuspendLayout();
+            this.tabHabitats.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnBack
@@ -57,7 +65,9 @@ namespace ZooBazaar_SAIA_Desktop {
             // 
             // tabEmployees
             // 
-            this.tabEmployees.Controls.Add(this.pvwGender);
+            this.tabEmployees.Controls.Add(this.pvwContract);
+            this.tabEmployees.Controls.Add(this.pvwWages);
+            this.tabEmployees.Controls.Add(this.pvwEmployeeAge);
             this.tabEmployees.Location = new System.Drawing.Point(4, 29);
             this.tabEmployees.Name = "tabEmployees";
             this.tabEmployees.Padding = new System.Windows.Forms.Padding(3);
@@ -66,20 +76,47 @@ namespace ZooBazaar_SAIA_Desktop {
             this.tabEmployees.Text = "Employees";
             this.tabEmployees.UseVisualStyleBackColor = true;
             // 
-            // pvwGender
+            // pvwContract
             // 
-            this.pvwGender.Location = new System.Drawing.Point(35, 39);
-            this.pvwGender.Name = "pvwGender";
-            this.pvwGender.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.pvwGender.Size = new System.Drawing.Size(368, 556);
-            this.pvwGender.TabIndex = 0;
-            this.pvwGender.Text = "plotView1";
-            this.pvwGender.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
-            this.pvwGender.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
-            this.pvwGender.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.pvwContract.Location = new System.Drawing.Point(31, 39);
+            this.pvwContract.Name = "pvwContract";
+            this.pvwContract.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwContract.Size = new System.Drawing.Size(297, 575);
+            this.pvwContract.TabIndex = 3;
+            this.pvwContract.Text = "plotView1";
+            this.pvwContract.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwContract.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwContract.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // pvwWages
+            // 
+            this.pvwWages.Location = new System.Drawing.Point(354, 39);
+            this.pvwWages.Name = "pvwWages";
+            this.pvwWages.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwWages.Size = new System.Drawing.Size(589, 283);
+            this.pvwWages.TabIndex = 2;
+            this.pvwWages.Text = "plotView1";
+            this.pvwWages.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwWages.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwWages.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // pvwEmployeeAge
+            // 
+            this.pvwEmployeeAge.Location = new System.Drawing.Point(354, 346);
+            this.pvwEmployeeAge.Name = "pvwEmployeeAge";
+            this.pvwEmployeeAge.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwEmployeeAge.Size = new System.Drawing.Size(589, 265);
+            this.pvwEmployeeAge.TabIndex = 1;
+            this.pvwEmployeeAge.Text = "plotView1";
+            this.pvwEmployeeAge.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwEmployeeAge.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwEmployeeAge.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
             // 
             // tabAnimals
             // 
+            this.tabAnimals.Controls.Add(this.pvwAge);
+            this.tabAnimals.Controls.Add(this.pvwPredator);
+            this.tabAnimals.Controls.Add(this.pvwHealth);
             this.tabAnimals.Location = new System.Drawing.Point(4, 29);
             this.tabAnimals.Name = "tabAnimals";
             this.tabAnimals.Size = new System.Drawing.Size(973, 646);
@@ -87,8 +124,45 @@ namespace ZooBazaar_SAIA_Desktop {
             this.tabAnimals.Text = "Animals";
             this.tabAnimals.UseVisualStyleBackColor = true;
             // 
+            // pvwAge
+            // 
+            this.pvwAge.Location = new System.Drawing.Point(18, 374);
+            this.pvwAge.Name = "pvwAge";
+            this.pvwAge.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwAge.Size = new System.Drawing.Size(925, 251);
+            this.pvwAge.TabIndex = 2;
+            this.pvwAge.Text = "plotView1";
+            this.pvwAge.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwAge.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwAge.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // pvwPredator
+            // 
+            this.pvwPredator.Location = new System.Drawing.Point(512, 17);
+            this.pvwPredator.Name = "pvwPredator";
+            this.pvwPredator.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwPredator.Size = new System.Drawing.Size(431, 340);
+            this.pvwPredator.TabIndex = 1;
+            this.pvwPredator.Text = "plotView1";
+            this.pvwPredator.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwPredator.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwPredator.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // pvwHealth
+            // 
+            this.pvwHealth.Location = new System.Drawing.Point(18, 17);
+            this.pvwHealth.Name = "pvwHealth";
+            this.pvwHealth.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwHealth.Size = new System.Drawing.Size(456, 340);
+            this.pvwHealth.TabIndex = 0;
+            this.pvwHealth.Text = "plotView1";
+            this.pvwHealth.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwHealth.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwHealth.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
             // tabHabitats
             // 
+            this.tabHabitats.Controls.Add(this.pvwHabitats);
             this.tabHabitats.Location = new System.Drawing.Point(4, 29);
             this.tabHabitats.Name = "tabHabitats";
             this.tabHabitats.Padding = new System.Windows.Forms.Padding(3);
@@ -96,6 +170,18 @@ namespace ZooBazaar_SAIA_Desktop {
             this.tabHabitats.TabIndex = 1;
             this.tabHabitats.Text = "Habitats";
             this.tabHabitats.UseVisualStyleBackColor = true;
+            // 
+            // pvwHabitats
+            // 
+            this.pvwHabitats.Location = new System.Drawing.Point(23, 75);
+            this.pvwHabitats.Name = "pvwHabitats";
+            this.pvwHabitats.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.pvwHabitats.Size = new System.Drawing.Size(923, 400);
+            this.pvwHabitats.TabIndex = 0;
+            this.pvwHabitats.Text = "plotView1";
+            this.pvwHabitats.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pvwHabitats.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pvwHabitats.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
             // 
             // Statistics
             // 
@@ -108,6 +194,8 @@ namespace ZooBazaar_SAIA_Desktop {
             this.Text = "Statistics";
             this.tabControl1.ResumeLayout(false);
             this.tabEmployees.ResumeLayout(false);
+            this.tabAnimals.ResumeLayout(false);
+            this.tabHabitats.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -119,6 +207,12 @@ namespace ZooBazaar_SAIA_Desktop {
         private System.Windows.Forms.TabPage tabEmployees;
         private System.Windows.Forms.TabPage tabAnimals;
         private System.Windows.Forms.TabPage tabHabitats;
-        private OxyPlot.WindowsForms.PlotView pvwGender;
+        private OxyPlot.WindowsForms.PlotView pvwContract;
+        private OxyPlot.WindowsForms.PlotView pvwWages;
+        private OxyPlot.WindowsForms.PlotView pvwEmployeeAge;
+        private OxyPlot.WindowsForms.PlotView pvwHealth;
+        private OxyPlot.WindowsForms.PlotView pvwAge;
+        private OxyPlot.WindowsForms.PlotView pvwPredator;
+        private OxyPlot.WindowsForms.PlotView pvwHabitats;
     }
 }
--- ZooBazaar-SAIA-Desktop/Statistics.cs
+++ ZooBazaar-SAIA-Desktop/Statistics.cs
@@ -7,12 +7,20 @@ using System.Text;
 using System.Windows.Forms;
 using Class_Library.Managers;
 using Class_Library;
+using Class_Library.Data_Access;
 using OxyPlot;
 using OxyPlot.Series;
+using OxyPlot.Axes;
 
 namespace ZooBazaar_SAIA_Desktop {
     public partial class Statistics : Form {
         private Employee loggedEmployee;
+
+        //Lists for storing data
+        List<Employee> employees;
+        List<Animal> animals;
+        List<Habitat> habitats;
+
         public Statistics(Employee e) {
             InitializeComponent();
             loggedEmployee = e;
@@ -22,20 +30,195 @@ namespace ZooBazaar_SAIA_Desktop {
         }
 
         private void CreateModels() {
-            //Create the following OxyPlot models
-            CreateGenderModel();
+            //Get all the needed data (employees, animals, and habitats)
+            EmployeeManager employeeManager = new EmployeeManager();
+            employees = employeeManager.GetAllEmployees();
+            AnimalManager animalManager = new AnimalManager();
+            animalManager.GetAllAnimals();
+            animals = animalManager.Animals;
+            HabitatManager habitatManager = new HabitatManager();
+            habitats = habitatManager.GetHabitats();
+            //Create the following OxyPlot models, which provide data for the graphs
+            CreateContractModel();
+            CreateWagesModel();
+            CreateEmpAgeModel();
+
+            CreateHealthModel();
+            CreatePredatorModel();
+            CreateAnimalAgeModel();
+
+            CreateHabitatModel();
         }
 
-        private void CreateGenderModel() {
-            //Pie chart of gender distribution of employees
-            PlotModel genderModel = new PlotModel { Title = "Gender Distribution" };
-            dynamic genderSeries = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
-            genderSeries.Slices.Add(new PieSlice("Americas", 929) { IsExploded = true });
-            genderSeries.Slices.Add(new PieSlice("Asia", 4157) { IsExploded = true });
+        private void CreateContractModel() {
+            //Count number of parttime/fulltime employees
+            int parttime = 0;
+            int fulltime = 0;
+            foreach (Employee e in employees) {
+                if (e.ContractType == "Part Time") {
+                    parttime++;
+                } else if (e.ContractType == "Full Time") {
+                    fulltime++;
+                }
+            }
+            //Pie chart of contract types
+            PlotModel contractModel = new PlotModel { Title = "Contract types" };
+            var contractSeries = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+            contractSeries.Slices.Add(new PieSlice("Part time", parttime) { IsExploded = true });
+            contractSeries.Slices.Add(new PieSlice("Full time", fulltime) { IsExploded = true });
 
-            genderModel.Series.Add(genderSeries);
+            contractModel.Series.Add(contractSeries);
+            pvwContract.Model = contractModel;
+        }
 
-            pvwGender.Model = genderModel;
+        private void CreateWagesModel() {
+            //Create list of wages
+            List<double> wages = new List<double>();
+            foreach (Employee e in employees) {
+                wages.Add(e.HourlyWage);
+            }
+            //Graph of hourly wages
+            PlotModel wagesModel = new PlotModel { Title = "Distribution of hourly wages" };
+            wagesModel.Axes.Add(new LinearAxis() { Title = "Hourly wage", Position = AxisPosition.Bottom });
+            wagesModel.Axes.Add(new LinearAxis() { Title = "Frequency", Position = AxisPosition.Left });
+            var wagesSeries = new HistogramSeries { FillColor = OxyColors.Green, StrokeColor = OxyColors.Black, StrokeThickness = 2 };
+            var bins = HistogramHelpers.CreateUniformBins(1, 50, 50);
+            var binningOptions = new BinningOptions(BinningOutlierMode.IgnoreOutliers, BinningIntervalType.InclusiveLowerBound, BinningExtremeValueMode.ExcludeExtremeValues);
+            var items = HistogramHelpers.Collect(wages, bins, binningOptions);
+
+            wagesSeries.Items.AddRange(items);
+            wagesModel.Series.Add(wagesSeries);
+            pvwWages.Model = wagesModel;
+        }
+
+        private void CreateEmpAgeModel() {
+            //Create list of ages
+            List<double> ages = new List<double>();
+            foreach (Employee e in employees) {
+                TimeSpan age = DateTime.Today - e.Birthdate;
+                int years = (age.Days / 365);
+                ages.Add(years);
+            }
+            //Graph of hourly wages
+            PlotModel agesModel = new PlotModel { Title = "Distribution of employee ages" };
+            agesModel.Axes.Add(new LinearAxis() { Title = "Age", Position = AxisPosition.Bottom });
+            agesModel.Axes.Add(new LinearAxis() { Title = "Frequency", Position = AxisPosition.Left });
+            var agesSeries = new HistogramSeries { FillColor = OxyColors.Blue, StrokeColor = OxyColors.Black, StrokeThickness = 2 };
+            var bins = HistogramHelpers.CreateUniformBins(15, 65, 50);
+            var binningOptions = new BinningOptions(BinningOutlierMode.IgnoreOutliers, BinningIntervalType.InclusiveLowerBound, BinningExtremeValueMode.ExcludeExtremeValues);
+            var items = HistogramHelpers.Collect(ages, bins, binningOptions);
+
+            agesSeries.Items.AddRange(items);
+            agesModel.Series.Add(agesSeries);
+            pvwEmployeeAge.Model = agesModel;
+        }
+
+        private void CreateHealthModel() {
+            //Count healthy and unhealthy animals
+            int healthy = 0;
+            int unhealthy = 0;
+            foreach (Animal a in animals) {
+                if (a.isInZoo == true) {
+                    if (a.health == "Healthy") {
+                        healthy++;
+                    } else {
+                        unhealthy++;
+                    }
+                }                
+            }
+            //Pie chart of health condition
+            PlotModel healthModel = new PlotModel { Title = "Health conditions" };
+            var healthSeries = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+            healthSeries.Slices.Add(new PieSlice("Healthy", healthy) { IsExploded = true });
+            healthSeries.Slices.Add(new PieSlice("Unhealthy", unhealthy) { IsExploded = true });
+
+            healthModel.Series.Add(healthSeries);
+            pvwHealth.Model = healthModel;
+        }
+
+        private void CreatePredatorModel() {
+            //Count predator and prey animals
+            int predator = 0;
+            int prey = 0;
+            foreach (Animal a in animals) {
+                if (a.isInZoo == true) {
+                    if (a.isPredator == true) {
+                        predator++;
+                    } else {
+                        prey++;
+                    }
+                }
+            }
+            //Pie chart of predator/prey division
+            PlotModel predatorModel = new PlotModel { Title = "Predator and Prey animals" };
+            var predatorSeries = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+            predatorSeries.Slices.Add(new PieSlice("Predator", predator) { IsExploded = true });
+            predatorSeries.Slices.Add(new PieSlice("Prey", prey) { IsExploded = true });
+
+            predatorModel.Series.Add(predatorSeries);
+            pvwPredator.Model = predatorModel;
+        }
+
+        private void CreateAnimalAgeModel() {
+            //Get list of animal ages
+            List<double> ages = new List<double>();
+            foreach (Animal a in animals) {
+                if (a.isInZoo == true) {
+                    TimeSpan age = DateTime.Today - a.birthday;
+                    int years = (age.Days / 365);
+                    ages.Add(years);
+                }
+            }
+            //Graph of animal ages
+            PlotModel agesModel = new PlotModel { Title = "Distribution of animal ages" };
+            agesModel.Axes.Add(new LinearAxis() { Title = "Age", Position = AxisPosition.Bottom });
+            agesModel.Axes.Add(new LinearAxis() { Title = "Frequency", Position = AxisPosition.Left });
+            var agesSeries = new HistogramSeries { FillColor = OxyColors.Orange, StrokeColor = OxyColors.Black, StrokeThickness = 2 };
+            var bins = HistogramHelpers.CreateUniformBins(0, 25, 26);
+            var binningOptions = new BinningOptions(BinningOutlierMode.IgnoreOutliers, BinningIntervalType.InclusiveLowerBound, BinningExtremeValueMode.ExcludeExtremeValues);
+            var items = HistogramHelpers.Collect(ages, bins, binningOptions);
+
+            agesSeries.Items.AddRange(items);
+            agesModel.Series.Add(agesSeries);
+            pvwAge.Model = agesModel;
+        }
+
+        private void CreateHabitatModel() {
+            //Get list of habitats, then get animals for each habitat
+            Dictionary<string, int> habitatsWithAnimals = new Dictionary<string, int>();
+            int animalCount = 0;
+            foreach (Habitat h in habitats) {
+                foreach (Animal a in animals) {
+                    if (a.habitat == h.ID) {
+                        animalCount++;
+                    }
+                }
+                if (animalCount > 0) {
+                    habitatsWithAnimals.Add(h.Title, animalCount);
+                }
+                animalCount = 0;
+            }
+            //Create lists for easier use in the graph
+            List<string> habitatList = new List<string>(); ;
+            List<int> animalsList = new List<int>(); ;
+            foreach (var ha in habitatsWithAnimals) {
+                habitatList.Add(ha.Key);
+                animalsList.Add(ha.Value);
+            }
+            //Graph of habitats, with the number of animals for each
+            var habitatModel = new PlotModel { Title = "Number of animals in each habitat" };
+            CategoryAxis catAxis = new CategoryAxis { Position = AxisPosition.Left };
+            foreach (string h in habitatList) {
+                catAxis.Labels.Add(h);
+            }
+            habitatModel.Axes.Add(catAxis);
+            var habitatSeries = new BarSeries();
+            foreach (int a in animalsList) {
+                habitatSeries.Items.Add(new BarItem(a));
+            }
+
+            habitatModel.Series.Add(habitatSeries);
+            pvwHabitats.Model = habitatModel;
         }
 
         private void btnBack_Click(object sender, EventArgs e) {
